e8408690222b3d09e261a21486805be7da3cb436
Victor Shin Kinoshita <v.kinoshita@murabei.com>
2016-09-27 22:42:02 -0300

    meus pedidos arrumado

--- front/src/app/pages/demand/myDemand/myDemand.controller.js
+++ front/src/app/pages/demand/myDemand/myDemand.controller.js
@@ -9,62 +9,132 @@
         var vm = this;
 
         // mock
-        vm.searchResult = createMock();
+        vm.waiting = createMock1();
+        vm.going = createMock2();
+        vm.finished = createMock3();
 
-        function createMock () {
+        function createMock1 () {
             var mocks = [];
             mocks = mocks.concat({
                 name: "medicamento A",
                 count: "12",
-                status: "em espera",
+                // status: "em espera",
                 date: new Date("2016-09-01")
             });
             mocks = mocks.concat({
                 name: "medicamento B",
                 count: "121",
-                status: "em espera",
+                // status: "em espera",
                 date: new Date("2016-09-02")
             });
             mocks = mocks.concat({
                 name: "medicamento C",
                 count: "312",
-                status: "em espera",
+                // status: "em espera",
                 date: new Date("2016-09-03")
             });
             mocks = mocks.concat({
                 name: "medicamento D",
                 count: "612",
-                status: "em andamento",
+                // status: "em andamento",
+                date: new Date("2016-09-04")
+            });
+            mocks = mocks.concat({
+                name: "medicamento D",
+                count: "612",
+                // status: "em andamento",
+                date: new Date("2016-09-04")
+            });
+
+            return mocks;
+        }function createMock2 () {
+            var mocks = [];
+            
+            mocks = mocks.concat({
+                name: "medicamento D",
+                count: "612",
+                // status: "em andamento",
                 date: new Date("2016-09-04")
             });
             mocks = mocks.concat({
                 name: "medicamento E",
                 count: "152",
-                status: "terminou",
+                // status: "terminou",
                 date: new Date("2016-09-05")
             });
             mocks = mocks.concat({
                 name: "medicamento F",
                 count: "9182",
-                status: "em espera",
+                // status: "em espera",
                 date: new Date("2016-09-06")
             });
-            mocks = mocks.concat({
-                name: "medicamento G",
-                count: "212",
-                status: "cancelado",
-                date: new Date("2016-09-07")
-            });
+            return mocks;
+        }
+        function createMock3 () {
+            var mocks = [];
+            
             mocks = mocks.concat({
                 name: "medicamento H",
                 count: "8162",
-                status: "terminou",
+                // status: "terminou",
                 date: new Date("2016-09-08")
             });
             mocks = mocks.concat({
                 name: "medicamento I",
                 count: "95612",
-                status: "cancelado",
+                // status: "cancelado",
+                date: new Date("2016-09-09")
+            });
+            
+            mocks = mocks.concat({
+                name: "medicamento H",
+                count: "8162",
+                // status: "terminou",
+                date: new Date("2016-09-08")
+            });
+            mocks = mocks.concat({
+                name: "medicamento I",
+                count: "95612",
+                // status: "cancelado",
+                date: new Date("2016-09-09")
+            });
+            
+            mocks = mocks.concat({
+                name: "medicamento H",
+                count: "8162",
+                // status: "terminou",
+                date: new Date("2016-09-08")
+            });
+            mocks = mocks.concat({
+                name: "medicamento I",
+                count: "95612",
+                // status: "cancelado",
+                date: new Date("2016-09-09")
+            });
+            
+            mocks = mocks.concat({
+                name: "medicamento H",
+                count: "8162",
+                // status: "terminou",
+                date: new Date("2016-09-08")
+            });
+            mocks = mocks.concat({
+                name: "medicamento I",
+                count: "95612",
+                // status: "cancelado",
+                date: new Date("2016-09-09")
+            });
+            
+            mocks = mocks.concat({
+                name: "medicamento H",
+                count: "8162",
+                // status: "terminou",
+                date: new Date("2016-09-08")
+            });
+            mocks = mocks.concat({
+                name: "medicamento I",
+                count: "95612",
+                // status: "cancelado",
                 date: new Date("2016-09-09")
             });
             return mocks;
