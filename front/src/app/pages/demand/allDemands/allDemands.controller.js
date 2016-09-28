97a0f03c5e647030562c8e7c845c3521e88bfb35
Victor Shin Kinoshita <v.kinoshita@murabei.com>
2016-09-28 00:12:41 -0300

    todos os pedidos

--- front/src/app/pages/demand/allDemands/allDemands.controller.js
+++ front/src/app/pages/demand/allDemands/allDemands.controller.js
@@ -7,10 +7,36 @@
 
   function allDemandsCtrl(){
     var vm = this;
-    
-    init();
 
-    function init(){
+    createMock();
+    function createMock() {
+      vm.featured = [];
+      vm.newest = [];
+      vm.searchResult = [];
+
+      var randR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      for (var i = 0; i < 12; i++) {
+        vm.featured = vm.featured.concat({
+          name: 'remedio ' + randR[Math.floor(Math.random()*randR.length)],
+          hospital: 'hospital ' + randR[Math.floor(Math.random()*randR.length)],
+          distance: Math.floor(Math.random()*100),
+          count: Math.floor(Math.random()*200)
+        });
+        vm.newest = vm.newest.concat({
+          name: 'remedio ' + randR[Math.floor(Math.random()*randR.length)],
+          hospital: 'hospital ' + randR[Math.floor(Math.random()*randR.length)],
+          distance: Math.floor(Math.random()*100),
+          count: Math.floor(Math.random()*200)
+        });
+      }
+      for (var i = 0; i < 30; i++) {
+        vm.searchResult = vm.searchResult.concat({
+          name: 'remedio ' + randR[Math.floor(Math.random()*randR.length)],
+          hospital: 'hospital ' + randR[Math.floor(Math.random()*randR.length)],
+          distance: Math.floor(Math.random()*100),
+          count: Math.floor(Math.random()*200)
+        });
+      } 
     }
 
   }
