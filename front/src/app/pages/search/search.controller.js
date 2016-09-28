0af0b8ab6e375a24f0c23e33020ac313b81ce40d
Gustavo Ehrhardt <gus.ehr@gmail.com>
2016-09-28 13:14:38 -0300

    lista de medicamentos

--- front/src/app/pages/search/search.controller.js
+++ front/src/app/pages/search/search.controller.js
@@ -5,23 +5,31 @@
         .module('BlurAdmin.pages.search')
         .controller('searchCtrl', searchCtrl);
 
-    function searchCtrl() {
+    function searchCtrl($timeout) {
         var vm = this;
 
         // mock
-        vm.searchResult = createMock();
+        vm.searchResult = [];
 
-        function createMock() {
-            var mocks = [];
-            for (var i = 0; i < 200; i++) {
-                var mock = {
-                    name: i,
-                    place: i*2,
-                    count: i*3
-                };
-                mocks = mocks.concat(mock); 
-            }
-            return mocks;
+        function initMedicamentos() {
+			// Initialize Firebase
+	  	    var config = {
+	  	      apiKey: "AIzaSyBmFPVlL3koC_PvD11mD_y7y0PmQZvm54s",
+	  	      authDomain: "medhub-3a1e3.firebaseapp.com",
+	  	      databaseURL: "https://medhub-3a1e3.firebaseio.com",
+	  	      storageBucket: "medhub-3a1e3.appspot.com",
+	  	      messagingSenderId: "542735452063"
+	  	    };
+	  	    firebase.initializeApp(config);
+
+	  		var estoqueRef = firebase.database().ref('medicamentos');
+	  	     estoqueRef.on('child_added', function(snapshot) {
+	  			 var item = snapshot.val();
+				 vm.searchResult.push(item);
+	  	     });
         }
+
+		initMedicamentos();
+		console.log(vm.searchResult.length);
     }
 } ());
